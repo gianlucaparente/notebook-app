90e8c5ac0d813eaafae7967d58713fcf5bc3287b
Gianluca Parente <gianluca.parente@objectway.it>
2018-03-13 12:20:27 +0100

    add method for search a contact

--- src/main/java/com/notebook/webservices/ContactRestController.java
+++ src/main/java/com/notebook/webservices/ContactRestController.java
@@ -16,23 +16,34 @@ public class ContactRestController {
 		this.contactRepository = contactRepository;
 	}
 
+	@CrossOrigin
 	@RequestMapping(method = RequestMethod.GET)
 	Collection<Contact> getContacts() {
 		System.out.println("ContactRestController :: endpoint '/contacts' called.");
 		return this.contactRepository.findAll();
 	}
 
+	@CrossOrigin
+	@RequestMapping(method = RequestMethod.GET, value="/search")
+	Collection<Contact> getContacts(@RequestParam String query) {
+		System.out.println("ContactRestController :: endpoint '/contacts/search' called with param query: " + query);
+		return this.contactRepository.findByNameContaining(query);
+	}
+
+	@CrossOrigin
 	@RequestMapping(method = RequestMethod.GET, value="/{id}")
 	Contact getContactById(@PathVariable Long id) {
 		System.out.println("ContactRestController :: endpoint '/contacts/" + id + "' called.");
 		return this.contactRepository.findOne(id);
 	}
 
+	@CrossOrigin
     @RequestMapping(value="/contact", method=RequestMethod.POST)
     Contact insertContact(@RequestBody Contact contact) {
         return this.contactRepository.save(contact);
     }
 
+	@CrossOrigin
     @RequestMapping(value="/contact", method=RequestMethod.PUT)
     Contact updateContact(@RequestBody Contact contact) {
         return this.contactRepository.save(contact);
